d2891b340f241697586da7d14ca1a49b613f6424
Necs <icaroibernon@live.com>
2020-12-16 00:45:51 -0300

    Pagina Base

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,45 @@
-import {useState} from 'react';
+import { useState } from "react";
+import Typical from "react-typical";
+import "tailwindcss/tailwind.css";
 
-function Home(){
-    return <div>
-        <h2>Contador</h2>
-        <Contador/>
+function Home() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <Corpo />
     </div>
+  );
 }
 
-function Contador(){
-    const [contador,setContador] = useState(1);
-
-    function adicionarContador(){
-        setContador(contador+1);
-    }
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
-    )
+function Navbar() {
+  return (
+    <div style={{ background: "#091c29" }}>
+      <div className="flex items-center justify-between w-11/12 mx-auto py-3 text-white">
+        <h1 className="text-2xl font-bold">Bem vindo!</h1>
+        <a href="#Contato">Contato</a>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+function Corpo() {
+  return (
+    <div
+      className="flex flex-grow h-full items-center justify-right text-white"
+      style={{ background: "#091c29" }}
+    >
+      <h1 className="mx-10 text-xl">
+        Olá meu nome é
+        <br />
+        <b>Ícaro Ibernon</b>
+        <br />e eu sou um: 
+      <Typical
+        steps={["Desenvolvedor", 750, "Desenvolvedor Web 📄", 500,"Desenvolvedor Backend 🔧", 500, "Desenvolvedor Mobile 📱", 500]}
+        loop={Infinity}
+      />
+      </h1>
+    </div>
+  );
+}
+
+export default Home;
